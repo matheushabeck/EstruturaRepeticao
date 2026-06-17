7e12ae9fa5608819c10a46747c0aa5c3b16efbf1
matheushabeck <matheushabeck@gmail.com>
2026-06-17 19:18:49 -0300

    test

---
+++ src/MatheusHoweHabeckL04E01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MatheusHoweHabeckL04E01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro positivo: ");
+        int quantidade = scanner.nextInt();
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.println("Bom dia");
+        }
+    }
+}
---
+++ src/MatheusHoweHabeckL04E02.java
@@ -0,0 +1,7 @@
+public class MatheusHoweHabeckL04E02 {
+    public static void main(String[] args) {
+        for (int numero = 1; numero <= 50; numero += 4) {
+            System.out.println(numero);
+        }
+    }
+}
